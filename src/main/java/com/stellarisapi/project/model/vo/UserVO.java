0579f74bbf12162786b0259ee1373a661429e1af
threeSeven <nuomibaicha@163.com>
2024-04-09 00:10:12 +0800

    fix 用戶返回信息過濾

--- src/main/java/com/stellarisapi/project/model/vo/UserVO.java
+++ src/main/java/com/stellarisapi/project/model/vo/UserVO.java
@@ -33,11 +33,6 @@ public class UserVO implements Serializable {
     private String userAvatar;
 
     /**
-     * 性别
-     */
-    private Integer gender;
-
-    /**
      * 用户角色: user, admin
      */
     private String userRole;
@@ -51,8 +46,6 @@ public class UserVO implements Serializable {
      * 更新时间
      */
     private Date updateTime;
-
-    // [编程导航学习圈](https://t.zsxq.com/0emozsIJh) 零基础快速入门编程，不走弯路！30+ 原创学习路线和专栏、1000+ 编程精华文章、500+ 编程学习指南、20T+ 编程资源汇总
 
     private static final long serialVersionUID = 1L;
 }
